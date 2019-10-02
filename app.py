ff7ab461974e816b0b2a03a6cd104653271a14b0
shailesh.pujari <pujarishailesh@gmail.com>
2019-10-02 13:35:42 -0400

    modified a bit to include looping over the list

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+CITIES = ['Boston', 'NewYork', 'Chicago']
+
+
 def run():
-    print("My Simple Python Application")
+    for i in range(0, 5):
+        print("My Simple Python Application")
+    for city in CITIES:
+        print(city)
 
 
 if __name__ == '__main__':
